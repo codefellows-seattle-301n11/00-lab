c956d14d7ba11c740c20dc8fead61c9d6c14b18b
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2018-07-22 16:19:19 -0700

    added template literal notation

--- 1-ES6-practice/ashley-breunich-1/js/app.js
+++ 1-ES6-practice/ashley-breunich-1/js/app.js
@@ -16,7 +16,7 @@ const votes = [];
 
 function Product(name) {
   this.name = name;
-  this.path = 'img/' + name + '.jpg';
+  this.path = `img/${name}.jpg`;
   this.votes = 0;
   this.views = 0;
   allProducts.push(this);
@@ -50,7 +50,7 @@ function displayPics(){
 
 function handleClick(event) {
   if (event.target.id === 'image_container') {
-    return alert('Be sure to click directly on an image!!');
+    return alert(`Be sure to click directly on an image!!`);
   }
   totalClicks += 1;
   if(totalClicks > 24) {
@@ -62,7 +62,7 @@ function handleClick(event) {
   for(let i = 0; i < names.length; i++){
     if(event.target.id === allProducts[i].name) {
       allProducts[i].votes += 1;
-      console.log(event.target.id + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views');
+      console.log(`${event.target.id} has ${allProducts[i].votes} votes in ${allProducts[i].views} views`);
     }
   }
   localStorage.busmall = JSON.stringify(allProducts);
@@ -73,7 +73,7 @@ function handleClick(event) {
 function showList() {
   for(let i = 0; i < allProducts.length; i++) {
     const liEl = document.createElement('li');
-    liEl.textContent = allProducts[i].name + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views';
+    liEl.textContent =`${allProducts[i].name} has ${allProducts[i].votes} votes in ${allProducts[i].views} views`;
     list.appendChild(liEl);
   }
 }
@@ -120,14 +120,14 @@ container.addEventListener('click', handleClick);
 
 document.getElementById('bus').addEventListener('click', function(){
   localStorage.removeItem('busmall');
-  console.log('Local storage was cleared!');
+  console.log(`Local storage was cleared!`);
 });
 
 if(localStorage.busmall){
-  console.log('Local storage data exists');
+  console.log(`Local storage data exists`);
   allProducts = JSON.parse(localStorage.busmall);
 } else {
-  console.log('There is no local storage data; initialize app by creating instances');
+  console.log(`There is no local storage data; initialize app by creating instances`);
   for(let i = 0; i < names.length; i++) {
     new Product(names[i]);
   }
